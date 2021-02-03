83145da2d8b59d5a8c1b9545fc6e990bc9142c15
Gary Maccabe <garymaccabe@Garys-MacBook-Pro.local>
2021-02-03 23:36:17 +0000

    Add passing tests for 15

--- FizzBuzzTests/BrainTests.swift
+++ FizzBuzzTests/BrainTests.swift
@@ -9,6 +9,7 @@ import XCTest
 @testable import FizzBuzz
 
 class BrainTests: XCTestCase {
+    let brain = Brain()
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -19,22 +20,25 @@ class BrainTests: XCTestCase {
     }
     
     func testIsDivisibleByThree() {
-        let brain = Brain()
         let result = brain.isDivisibleByThreeAndFive(number: 3)
         
         XCTAssertEqual(result, true)
     }
     
     func testIsNotDivisibleByOne() {
-        let brain = Brain()
         let result = brain.isDivisibleByThreeAndFive(number: 1)
         
         XCTAssertEqual(result, false)
     }
     
     func testIsDivisibleByFive() {
-        let brain = Brain()
         let result = brain.isDivisibleByThreeAndFive(number: 5)
+        
+        XCTAssertEqual(result, true)
+    }
+    
+    func testIsDivisibleByFifteen() {
+        let result = brain.isDivisibleByThreeAndFive(number: 15)
         
         XCTAssertEqual(result, true)
     }
